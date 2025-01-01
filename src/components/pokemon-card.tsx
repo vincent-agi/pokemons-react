e938f12f3ac95d28e8fed34c474bc95f1e6b605f
Vincent AGI <vincent.agi.96@gmail.com>
2025-01-01 14:45:54 +0100

    ajout type pokemon avec couleurs

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -7,6 +7,49 @@ type Props= {
 
 const PokemonCard: FunctionComponent<Props> = ({pokemon}) => {
 
+    const formatType = (type:string) => {
+        let color: string;
+        switch(type) {
+            case 'Feu':
+                color = 'red';
+                break;
+            case 'Eau':
+                color = 'blue';
+                break;
+            case 'Plante':
+                color = 'green';
+                break;
+            case 'Insecte':
+                color = 'brown';
+                break;
+            case 'Normal':
+                color = 'grey';
+                break;
+            case 'Vol':
+                color = 'blue';
+                break;
+            case 'Poison':
+                color = 'purple';
+                break;
+            case 'Fée':
+                color = 'pink';
+                break;
+            case 'Psy':
+                color ='purple'
+                break;
+            case 'Électrique':
+                color = 'lime';
+                break;
+            case 'Combat':
+                color = 'orange';
+                break;
+            default:
+                color = 'grey';
+                break;
+        }
+        return `inline-flex items-center rounded-md bg-${color}-50 px-2 py-1 text-xs font-medium text-${color}-700 ring-1 ring-inset ring-${color}-700/10`;
+    }
+
     return(
         <div className="flex justify-evenly">
             <div className="w-64 bg-white border border-gray-200 rounded-lg shadow bg-withe-300 dark:border-gray-700 mx-8">
@@ -20,7 +63,9 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon}) => {
                     <ul className="mb-3 font-normal text-gray-100 dark:text-gray-400">
                         <li>Vie : {pokemon.hp}</li>
                         <li>Pouvoir : {pokemon.cp}</li>
-                        <li>Type : {pokemon.types.toString()}</li>
+                        <li>Type : {pokemon.types.map((type) => (
+                            <span className={formatType(type)} key={type}>{type}</span>
+                        ))}</li>
                         <li>Créer le : {pokemon.created.toLocaleDateString()}</li>
                     </ul>
                 </div>
